f963809df349322fc67d486ea48d87eeda5a6ca0
tulip2201 <tiu.tarafdar@gmail.com>
2021-08-30 13:02:35 +0530

    first commit

---
+++ springboot-jenkins-test/src/main/java/com/example/springbootjenkinstest/SpringbootJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkinstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsTestApplication.class, args);
+	}
+
+}
---
+++ springboot-jenkins-test/src/test/java/com/example/springbootjenkinstest/SpringbootJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
